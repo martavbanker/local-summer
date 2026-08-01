93dd822f9a6902265892ff0c2ad877c3cb0de917
martavbanker <mvalerdelportillo@gmail.com>
2026-08-01 16:41:23 -0700

    Remove file extension from Header import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from './Header.tsx';
+import { Header } from './Header';
 import { Hero } from './Hero';
 import { Pillars } from './Pillars';
 import { WhySpain } from './WhySpain';
